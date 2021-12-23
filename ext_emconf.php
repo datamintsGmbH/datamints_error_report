244b96750b173efa38945130ef219ee73b20035b
Mark Weisgerber <m.weisgerber@datamints.com>
2021-12-23 16:12:23 +0100

    R-31660: Nach Tests die Freigabe für TYPO3 11.x freigegeben

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
