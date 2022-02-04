36cca87722436afd0f0da52d4b8c1cccda7ea5f2
Mark Weisgerber <m.weisgerber@datamints.com>
2022-02-04 08:29:56 +0100

    small fixes

--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,9 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-call_user_func(static function() {
+call_user_func(static function () {
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('datamints_error_report', 'Configuration/TypoScript', 'datamints Error Report');
+    // Currently, no TS required for this extension
+    //\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('datamints_error_report', 'Configuration/TypoScript', 'datamints Error Report');
 
 });
