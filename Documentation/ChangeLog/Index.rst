f13c72ceb15dbd1fe7d5bf86997c7afc5f42c1f8
Mark Weisgerber <m.weisgerber@datamints.com>
2022-02-07 08:29:11 +0100

    added services.yaml: command dependency injection for TYPO3 11.x increased version number

--- Documentation/ChangeLog/Index.rst
+++ Documentation/ChangeLog/Index.rst
@@ -7,6 +7,11 @@ Change log
 ==========
 
 
+Version 1.0.4
+-------------
+
+added services.yaml: command dependency injection for TYPO3 11.x
+
 Version 1.0.3
 -------------
 
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,13 +2,13 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'datamints Error Report',
-    'description' => 'Sends bundled error-log entries via email',
+    'description' => 'Adds scheduler task to send bundled sys_log errors via email',
     'category' => 'services',
     'author' => 'Mark Weisgerber',
     'author_email' => 'm.weisgerber@datamints.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.3',
+    'version' => '1.0.4',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.9.99',
